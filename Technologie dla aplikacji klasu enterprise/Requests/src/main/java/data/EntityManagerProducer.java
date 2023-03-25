a5120d275bc3e8bcbd70ea86256d58318506d6af
qwetroman <77645381+qwetroman@users.noreply.github.com>
2023-03-25 11:31:46 +0100

    Lab3

---
+++ Technologie dla aplikacji klasu enterprise/Requests/src/main/java/data/EntityManagerProducer.java
@@ -0,0 +1,19 @@
+package data;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+public class EntityManagerProducer {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Produces
+    @RequestScoped
+    public EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
