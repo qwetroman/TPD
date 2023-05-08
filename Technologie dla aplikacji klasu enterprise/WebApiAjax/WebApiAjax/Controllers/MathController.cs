54ff4267b5db10f68f0a258774951b61b0bf415e
qwetroman <77645381+qwetroman@users.noreply.github.com>
2023-05-08 21:53:41 +0200

    Ajax

---
+++ Technologie dla aplikacji klasu enterprise/WebApiAjax/WebApiAjax/Controllers/MathController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAjax.Models;
+
+namespace WebApiAjax.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        // GET api/math
+        [HttpGet]
+        public CalcResult Calculate(int x, int y)
+        {
+            return new CalcResult()
+            {
+                Sum = x + y,
+                Difference = x - y,
+                Product = x * y,
+                Quotient = x / y
+            };
+        }
+    }
+}
